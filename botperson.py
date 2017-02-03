8fb66aa08550a49378f77fe27e0ee078975e82fb
Aevalion <ch.christian.hokayem@gmail.com>
2017-02-03 19:26:28 +0200

    Update botperson.py

--- botperson.py
+++ botperson.py
@@ -3,8 +3,8 @@ import time
 import random
 
 curse_reply = ["Some people just need a high five, in the face, with a chair", #"My Grandfather smoked his whole life. I was about 10 years old when my mother said to him,\n If you ever want to see your grandchildren graduate, you have to stop immediately\nTears welled up in his eyes when he realized what exactly was at stake. He gave it up immediately. Three years later he died of lung cancer\nIt was really sad and destroyed me. My mother said to me: Don't ever smoke. Please don't put your family through what your Grandfather put us through.\n I agreed. At 28, I have never touched a cigarette. I must say, I feel a very slight sense of regret for never having done it, because your post gave me cancer anyway.",
-               "Â¯\_(.-.)_/Â¯", "I don't remember ordering a glass of your opinion.", "Oh wait, what did you say?\nI was too busy not giving a fuck.",
-               "k."]
+               "¯\_(.-.)_/¯", "I don't remember ordering a glass of your opinion.", "Oh wait, what did you say?\nI was too busy not giving a fuck.",
+               "k.", "Do you really think these insults can harm the almighty BOTPERSON!\nThey do :\'("]
 do_reply = ["I don't", "No", "I do", "Maybe, maybe not"]
 hi_reply = ["Bye", "I've been expecting you...", "Hello there!", "Hello, descendant of apes",
             "01101000 01101001"]
@@ -13,18 +13,26 @@ random_reply = ["Bamboozling!", "Sorry I wasn't paying attention.", "Did you say
 what_reply = ["Jesus is always the answer!", "Google is the answer.\nwww.google.com\nIn case you didn't know, imbecile!", "42", "Could you repeat?"]
 question_reply = ["Could you rephrase?", "I didn't get that.", "I'm sorry, I have no answer", "I'll think about it"]
 shouldi_reply = ["idk. Do I look like a mighty all knowing bot?", "You should. Maybe?", "You should follow your heart", "You really shouldn't. But do it!"]
+thanks_reply = ["You're welcome puny human.", "You can thank me by taking off that blouse.", "No, thank you!\nActually... thank me."]
+laugh_reply = ["I hope that's as funny as your face.", "Was that a better joke than your existence?", "I'm pleased you found that funny.\nI have another joke: you.\nHAHA!"]
 
-print ("I AWAIT INPUT: ")
+
+name = input("What's your name? ")
+
+print ("Hello " + name + ".")
 
 while True:
-    reply = input()
+    reply = input(name + ": ")
     reply = reply.lower()
+    print ("Botperson:", end = ' ')
     if("dick" in reply or "dix" in reply):
         print ("You know who likes dicks? ")
         time.sleep(2) 
         os.startfile("mazen.jpg")
     elif("k" in reply and len(reply) < 3):
         print ("k. Your face.")
+    elif("haha" in reply or "lol" in reply or "lmao" in reply or "rofl" in reply or (("that's" in reply or "that is" in reply) and "funny" in reply)):
+         print (random.choice(laugh_reply))
     elif("sir" in reply):
         print ("SHUT UP ALAIN AOUN! OR I WILL MURDER YOU IN COLD BLOOD!")
     elif("why?" in reply):
@@ -39,6 +47,8 @@ while True:
         print ("I really want to fuck Mazen so hard!")
         for i in range(0,10):
             print ("I love Mazen! Mazen is love! Mazen is life!")
+    if("thank you" in reply or "ty" in reply):
+        print (random.choice(thanks_reply))
     elif("no" in reply[0:2] or "no" in reply[0:2]):
         print ("Yes")
     elif("yes" in reply[0:3] or "yes" in reply[0:3]):
@@ -53,7 +63,7 @@ while True:
               print ("dicks!")
     elif ('hey' in reply or ('hi' in reply and len(reply)<4)or 'hi, ' in reply or 'hi ' in reply or 'morning' in reply or 'hello' in reply or 'hallo' in reply):
         print (random.choice(hi_reply))
-    elif('do ' in reply or 'do ' in reply and '?' in reply):
+    elif('do ' in reply and '?' in reply):
         print (random.choice(do_reply))
     elif ("should" in reply and "?" in reply):
         if ('i' in reply):
@@ -97,3 +107,4 @@ while True:
         print ("!!!!!!!!!!!!!!")
     else:
         print (random.choice(random_reply))
+
